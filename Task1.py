300d32fd4dd54a0650a40feb28e010d0e516d807
hossam abu elfetuoh <hossam1115@gmail.com>
2025-10-04 07:02:26 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+#1-Define variables for the prices of three items
+apple=50
+orange=40
+milk=30
+#2-Define a variable for the total budget
+budget=200
+#3-Calculate the total cost of the items
+total=apple+orange+milk
+#4-Compare the total cost with the budget
+print("total price items:",total)
+print("total budget",budget)
+#5-Calculate the difference (either how much money is left or how much more is needed)
+money_left=budget-total
+print("money left:", money_left)
